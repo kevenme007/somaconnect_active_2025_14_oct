8a13855308d31c69a352de943a8ccfc03e53367e
keven.mgumba <kevenraymond77@gmail.com>
2026-04-18 15:08:46 +0300

    sitemap commented

--- resources/views/includes/frontend1/footer.blade.php
+++ resources/views/includes/frontend1/footer.blade.php
@@ -70,13 +70,13 @@
                         Copyright © {{ date('Y') }} <a href="/about">Soma Connect</a>. All Rights Reserved.
                     </p>
                 </div>
-                <div class="col-md-6">
+                {{-- <div class="col-md-6">
                     <ul class="footer-bottom-links">
                         <li><a href="/privacy">Privacy Policy</a></li>
                         <li><a href="/terms">Terms of Service</a></li>
                         <li><a href="/sitemap">Sitemap</a></li>
                     </ul>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
